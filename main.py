5f9162188b0648d9512f0700235ee28466b18ff4
Abhinav <134328872+Invictus27@users.noreply.github.com>
2024-04-13 13:43:45 +0530

    Update and rename KrakenScanner.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socket
 
-# variable fir colored text
+# variable for colored text
 blue = "\033[94m"
 green = "\033[92m"
 red = "\033[91m"
@@ -20,8 +20,8 @@ def Scan(ip, port):
 # List of common ports to scan
 common_ports = [21, 22, 80, 443, 8080, 1433, 5060, 137, 139, 162, 161, 53, 23, 3306, 5432]
 
-print(f"{blue}######KrakenScanner#######{normal}")
-print(f"{blue}[*]Credit: Morta1DieTw1Ce{normal}")
+print(f"{blue}######Port-Scanner#######{normal}")
+print(f"{blue}[*]Credit: Invictus27{normal}")
 print(f"{blue}[*] This tool is for educational purposes only.{normal}")
 print(f"{blue}[*] The developer does not support illegal use of this script.{normal}")
 ip = input(f"{blue}[*] Enter IP address of the target: {normal}")
